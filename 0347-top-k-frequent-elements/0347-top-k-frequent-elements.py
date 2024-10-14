b3f84ea98ca153340eeb039b12c80ff340997c4e
Muna Yusuf <85841496+Muna-Yusuf@users.noreply.github.com>
2024-10-14 23:15:22 +0300

    Time: 72 ms (76.23%), Space: 14.9 MB (71.93%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        comman = count.most_common(k)
+
+        return [elem for elem, frequency in comman]
+
+
+        
