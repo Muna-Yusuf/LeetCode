ca22dd93a6b3432854495b3e32a469b74674e9d8
Muna Yusuf <85841496+Muna-Yusuf@users.noreply.github.com>
2024-10-24 19:07:45 +0300

    Time: 59 ms (12.85%), Space: 19.8 MB (34.32%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,17 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        current = root
+        
+        while current:
+            if p.val < current.val and q.val < current.val:
+                current = current.left
+            elif p.val > current.val and q.val > current.val:
+                current = current.right
+            else:
+                return current
